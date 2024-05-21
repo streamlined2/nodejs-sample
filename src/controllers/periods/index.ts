4f043e7311a4bcc084d9968a9a5698665ded92e6
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2024-05-21 20:14:39 +0300

    Person existence check
    Added validation check if person exists;
    Validation checks moved from controller to service layer.

--- src/controllers/periods/index.ts
+++ src/controllers/periods/index.ts
@@ -1,16 +1,13 @@
 import log4js from 'log4js';
 import { Request, Response } from 'express';
-import { minDate, PeriodSaveDto, remarkMinLength } from 'src/dto/period/periodSaveDto';
+import { PeriodSaveDto } from 'src/dto/period/periodSaveDto';
 import { savePeriod as savePeriodApi } from 'src/services/period';
 import httpStatus from 'http-status';
 import { InternalError } from 'src/system/internalError';
-import { MissingPersonIdError } from 'src/system/missingPersonIdError';
-import { InvalidDataError } from 'src/system/invalidDataError';
 
 export const savePeriod = async (request: Request, response: Response) => {
     try {
         const dto = new PeriodSaveDto(request.body);
-        checkInputData(dto);
         const id = await savePeriodApi(dto);
         response.status(httpStatus.CREATED).send({ id });
     } catch (error) {
@@ -19,29 +16,3 @@ export const savePeriod = async (request: Request, response: Response) => {
         response.status(status).send({ message });
     }
 };
-
-const checkInputData = (dto: PeriodSaveDto) => {
-    if (!dto.isPersonIdValid()) {
-        throw new InvalidDataError(`Person id should be valid positive number`);
-    }
-    if (!dto.isPeriodTypeValid()) {
-        throw new InvalidDataError(`Period type should be Studying, MilitaryService, Working, Entrepreneurship, CareerBreak`);
-    }
-    if (!dto.isStartValid()) {
-        throw new InvalidDataError(`Period start date should be greater ${minDate}`);
-    }
-    if (!dto.isFinishValid()) {
-        throw new InvalidDataError(`Period finish date should be greater than start date ${dto.getStart()}`);
-    }
-    if (!dto.isRemarkValid()) {
-        throw new InvalidDataError(`Remark should not be empty string data and at least of ${remarkMinLength} characters`);
-    }
-    if (!checkIfPersonIdPresent(dto)) {
-        throw new MissingPersonIdError(`No person present with id ${dto.getPersonId()}`);
-    }
-}
-
-const checkIfPersonIdPresent = (dto: PeriodSaveDto): boolean => {
-    //TODO check if dto.periodId is valid and present by fetching data from another microservice
-    return dto.getPersonId() !== undefined;
-}
--- src/services/period/index.ts
+++ src/services/period/index.ts
@@ -1,8 +1,15 @@
 import PeriodModel from 'src/model/period';
-import { PeriodSaveDto } from 'src/dto/period/periodSaveDto';
+import { MissingPersonIdError } from 'src/system/missingPersonIdError';
+import { InvalidDataError } from 'src/system/invalidDataError';
+import { minDate, PeriodSaveDto, remarkMinLength } from 'src/dto/period/periodSaveDto';
+import log4js from 'log4js';
+import httpStatus from 'http-status';
+
+const personFetchEndpoint: string = 'http://localhost:8080/api/person/';
 
 export const savePeriod = async (
     dto: PeriodSaveDto): Promise<string> => {
+    await checkInputData(dto);
     const document = await new PeriodModel(
         {
             personId: dto.getPersonId(),
@@ -13,3 +20,35 @@ export const savePeriod = async (
         }).save();
     return document._id;
 };
+
+const checkInputData = async (dto: PeriodSaveDto): Promise<void> => {
+    if (!dto.isPersonIdValid()) {
+        throw new InvalidDataError(`Person id should be valid positive number`);
+    }
+    if (!dto.isPeriodTypeValid()) {
+        throw new InvalidDataError(`Period type should be Studying, MilitaryService, Working, Entrepreneurship, CareerBreak`);
+    }
+    if (!dto.isStartValid()) {
+        throw new InvalidDataError(`Period start date should be greater ${minDate}`);
+    }
+    if (!dto.isFinishValid()) {
+        throw new InvalidDataError(`Period finish date should be greater than start date ${dto.getStart()}`);
+    }
+    if (!dto.isRemarkValid()) {
+        throw new InvalidDataError(`Remark should not be empty string data and at least of ${remarkMinLength} characters`);
+    }
+    const isPresent = await personIsPresent(dto);
+    if (!isPresent) {
+        throw new MissingPersonIdError(`No person present with id ${dto.getPersonId()}`);
+    }
+}
+
+const personIsPresent = async (dto: PeriodSaveDto): Promise<boolean> => {
+    try {
+        const response = await fetch(personFetchEndpoint + dto.getPersonId());
+        return response.status == httpStatus.OK;
+    } catch (error) {
+        log4js.getLogger().error('Error while checking if person exists', error);
+        return false;
+    }
+}
